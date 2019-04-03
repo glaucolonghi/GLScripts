50c5bca0a5728772f2ea60ff9f9cc99b926ad96b
glaucolonghi <44718722+glaucolonghi@users.noreply.github.com>
2019-04-03 09:17:34 -0700

    Optimizing Code +-

--- transferAttributesMultiple.py
+++ transferAttributesMultiple.py
@@ -7,34 +7,24 @@
 import sys
 import maya.cmds as mc
 
-Values = dict(valueA = 1, valueB = 1, VertexPosition = 0, option2 = False, TransferPosition = 0, VertexPositionB = None)
+Values = dict(TransferPosition = 0, VertexPositionB = None, TransferUVs = 0, TransferUVB = None)
 
 
 def printCommand(*args):
 
     print Values
 
-def VP(self):
+def TransferVP(*args):
 
     Values["TransferPosition"] = 1
     mc.button(Values["VertexPositionB"], edit=True, enable = False)
-    print "value A has changed to 1"
+    print "TransferVertexPositionActive"
 
- 
-def changeTo1B(*args):
+def TransferUV(*args):
 
-    Values["valueB"] = 20
- 
-    visibility = mc.button(Values["option2"], query=True, enable=True)
- 
-    if visibility:
-        visibility = not visibility
- 
-    mc.button(Values["option2"], edit=True, enable= visibility)
- 
-    print "value B has changed to 20"
- 
-
+    Values["TransferUVs"] = 1
+    mc.button(Values["TransferUVB"], edit=True, enable = False)
+    print "TransferUV Active"
 
 def menuWindow():
 
@@ -44,20 +34,20 @@ def menuWindow():
     menu = mc.window("menuGL")
     mc.columnLayout()
 #    mc.paneLayout(configuration="quad")
-    Values["VertexPositionB"] = mc.button(label="VertexPosition", c=VP, enable=True)
-    Values["option2"] = mc.button(label="Option02", c=changeTo1B, enable=True)
-    mc.button(label="Print", command=printCommand)
+    Values["VertexPositionB"] = mc.button(label="VertexPosition", c=TransferVP, enable=True)
+    Values["TransferUVB"] = mc.button(label="TransferUV", c=TransferUV, enable=True)
     mc.button(label="RESET", command=reset)        
     mc.button(label="TransferAttributes", command=tas)
+    mc.button(label="DEBUG", command=printCommand)
     mc.showWindow(menu)
  
 def reset(*args):
 
-    Values["valueA"] = 0
-    Values["valueB"] = 0  
+    Values["TransferPosition"] = 0
+    Values["TransferUVs"] = 0
 
-    mc.button(Values["VertexPosition"], edit=True, enable= True)
-    mc.button(Values["option2"], edit=True, enable= True)
+    mc.button(Values["VertexPositionB"], edit=True, enable= True)
+    mc.button(Values["TransferUVB"], edit=True, enable= True)
  
     print "values and buttons have been reset"
 
@@ -67,27 +57,21 @@ def tas(*args):
 #select master object first
 
     # sel is = all selected
-
     sel = mc.ls(sl = True)
-
     # len returns the list values of an object - sel in this case
-
     if len(sel) <= 1:
         sys.exit('Please select at least 2 different polygonal objects.\n')
-
     # selectionMask 12 to make sure you select polygons
-
     selType = mc.filterExpand(selectionMask = 12)
     if selType == None:
         sys.exit("You did not select an polygonal object.\n")
-
     #for loop - x in range - starting on 1 because we are saving 0 as master, then running our sel list
-
     for x in range(1, len(sel)):
         mc.select(sel[0])
         mc.select(sel[x], add = True)
-        mc.transferAttributes(sel[0], sel[x], transferPositions=Values["TransferPosition"], transferNormals=0, transferUVs=0, transferColors=0, sampleSpace=3, searchMethod=3, targetUvSet="map2", sourceUvSet="map1")
+        mc.transferAttributes(sel[0], sel[x], transferPositions=Values["TransferPosition"], transferNormals=0, transferUVs=Values["TransferUVs"], transferColors=0, sampleSpace=3, searchMethod=3, targetUvSet="map2", sourceUvSet="map1")
     # not necessary the use of else here
+    else:
         if Values["TransferPosition"] == 1:
             print "deu certo"
             sys.stdout.write('Your Verts were transferred.\n')
@@ -101,10 +85,23 @@ def tas(*args):
 
             congratsMenu = mc.window("congrats")
             mc.paneLayout()
-            mc.button(label="DEU CERTO", command=delCongrats)
+            mc.button(label="VERTEX POSITION WAS TRANSFERED", command=delCongrats)
             mc.showWindow(congratsMenu)
 
 
-        if Values["TransferPosition"] == 0:
-            print "deu caca"
+        if Values["TransferUVs"] == 1:
+            print "Transfered UVS"
+            sys.stdout.write('Your UVS were transferred.\n')
+            mc.deleteUI("menuGL")
+
+            if mc.window("congratsUVS", query=True, exists=True):
+                mc.deleteUI("congratsUVS", window=True)
+
+            def delCongrats(*args):
+                mc.deleteUI("congratsUVS", window=True)
+
+            congratsMenuUVS = mc.window("congratsUVS")
+            mc.paneLayout()
+            mc.button(label="UVS WERE TRANSFERED :)", command=delCongrats)
+            mc.showWindow(congratsMenuUVS)
 
